786420dce07aaa786d059a894b9e8a2447c8d51b
Rakesh K <rakesh.ravindran.scorpion@gmail.com>
2021-04-02 01:44:03 +0200

    Removed default app component contents and added a component.

--- SimpleLoanCalculator/src/app/app.module.ts
+++ SimpleLoanCalculator/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoanCalculatorComponent } from './loan-calculator/loan-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoanCalculatorComponent
   ],
   imports: [
     BrowserModule,
