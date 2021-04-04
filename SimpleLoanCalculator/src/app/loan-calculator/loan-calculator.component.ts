6b0709110ce60e09e97bf34aecdde4a9637ea94f
Rakesh K <rakesh.ravindran.scorpion@gmail.com>
2021-04-04 13:45:57 +0200

    Added logic for decimal point calculation

--- SimpleLoanCalculator/src/app/loan-calculator/loan-calculator.component.ts
+++ SimpleLoanCalculator/src/app/loan-calculator/loan-calculator.component.ts
@@ -11,8 +11,8 @@ export class LoanCalculatorComponent implements OnInit {
   title : string = "Simple Loan Calculator";
   inputForm: FormGroup;
 
-  minimumMonthlyIncome: number = 500000;
-  minimumRequestedAmount: number = 20000000;
+  minimumMonthlyIncome: number = 500.000;
+  minimumRequestedAmount: number = 20000.000;
   childrenArray = ['NONE', 'SINGLE', ' MULTIPLE'];
   coapplicantArray = ['NONE', 'SINGLE_BORROWER', ' MULTIPLE_BORROWERS'];
 
@@ -28,34 +28,68 @@ export class LoanCalculatorComponent implements OnInit {
     });
   }
 
-  removeComma(value:string) : string {
-    return (value == null)? "":value.replace(/[^0-9]+/g,'');
+  removeOtherCharacters(value:string) : string { 
+    let decimalDotExist = false;
+    let index = 0;
+    if(value != null) { 
+      console.log("removeOtherCharacters:b4value =",value);     
+      if(value.indexOf('.')> -1) {
+        index = value.indexOf('.');
+        decimalDotExist = true;
+        console.log("removeOtherCharacters:b4value index =",index);
+      
+        console.log("removeOtherCharacters:value =",value);
+        
+        //before and after length from the first decimal point found from right side
+        //newValue is the value till first dot and will be used for length calculation
+        let newValue = value.slice(0,index);
+        let beforeLength = newValue.length;
+        console.log("b4 length =",beforeLength);
+        newValue = (newValue == null)? "":newValue.replace(/[^0-9]+/g,'');
+        let afterLength = newValue.length;
+        console.log("after length =",afterLength);
+        //placing decimal logic ends
+
+        value = (value == null)? "":value.replace(/[^0-9]+/g,'');
+        if(index>0 && beforeLength != afterLength) {
+          let diff = beforeLength-afterLength;
+          index = index - diff ;
+        }
+        if(decimalDotExist) {
+          decimalDotExist = false;        
+          value = value.slice(0,index)+'.'+value.slice(index,value.length+1);
+          index = 0;
+        }
+      }      
+    }
+    console.log("removeOtherCharacters:aftervalue =",value);
+    return value;
   }
 
   changeMonthlyIncomeFormat() {
     let inputValue = this.inputForm.get('monthlyIncome').value;
     console.log("changeToDecimalFormat: inputValue = ",inputValue);
     if(inputValue){
-      inputValue = this.removeComma(inputValue);
+      inputValue = this.removeOtherCharacters(inputValue);
       let income: string = this.decimalPipe.transform(inputValue);
       this.inputForm.get('monthlyIncome').setValue(income, {emitEvent: false });
     }
   }
 
   onSubmit() {
-    
+
     console.log(this.inputForm);
   }
   
   validationForMontlyIncome(control: FormControl) : {[s: string]: boolean}{
-    let inputValue = this.removeComma(control.value);
+    let inputValue = this.removeOtherCharacters(control.value);
     console.log("validationForMontlyIncome: inputValue =", inputValue);
     if(inputValue) {
       let inputNumber: number = Number(inputValue);
       if(inputNumber<this.minimumMonthlyIncome) {
         return {'minimumMonthlyIncomeError': true};
       }
-    }  
+    } 
     return null;
   }
 
@@ -63,14 +97,14 @@ export class LoanCalculatorComponent implements OnInit {
     let inputValue = this.inputForm.get('requestedAmount').value;
     console.log("changeRequestedAmountFormat: inputValue = ",inputValue);
     if(inputValue){
-      inputValue = this.removeComma(inputValue);
+      inputValue = this.removeOtherCharacters(inputValue);
       let income: string = this.decimalPipe.transform(inputValue);
       this.inputForm.get('requestedAmount').setValue(income, {emitEvent: false });
     }
   }
 
   validationForRequestedAmount(control: FormControl) : {[s: string]: boolean}{
-    let inputValue = this.removeComma(control.value);
+    let inputValue = this.removeOtherCharacters(control.value);
     console.log("validationForRequestedAmount : inputValue =", inputValue);
     if(inputValue) {
       let inputNumber: number = Number(inputValue);
